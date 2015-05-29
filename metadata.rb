29edc197302eb970faeb157bb243201509a34cf6
Chad Wilken <chad.wilken@gmail.com>
2015-05-29 16:18:28 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name              "rails_gem_dependencies-tlq"
-maintainer        "Ben Dixon"
-maintainer_email  "ben@hillsbede.co.uk"
+name              "rails_gem_dependencies-ctw"
+maintainer        "Chad Wilken"
+maintainer_email  "me@chadwilken.com"
 description       "Installs packages commonly required by rails gems"
 version           "0.0.3"
 
-recipe "rails_gem_dependencies-tlq", "all the gems"
+recipe "rails_gem_dependencies-ctw", "all the gems"
 
 supports "ubuntu"
 
